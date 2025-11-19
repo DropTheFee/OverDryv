23492229367916c8860274fb12fee50e8fd6c9b9
DropTheFee <Steve@DropTheFee.com>
2025-11-19 10:34:13 -0600

    Remove marketing pages from main branch

--- Downloads/AutomotiveCRM-main/src/App.tsx
+++ Downloads/AutomotiveCRM-main/src/App.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import Layout from './components/layout/Layout';
-import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import CheckInPage from './pages/CheckInPage';
-import PricingPage from './pages/PricingPage';
 import InvoiceDemo from './pages/InvoiceDemo';
 import AdminDashboard from './pages/AdminDashboard';
 import CustomerPortal from './pages/CustomerPortal';
@@ -15,10 +13,8 @@ function App() {
     <AuthProvider>
       <Layout>
         <Routes>
-          <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/check-in" element={<CheckInPage />} />
-          <Route path="/pricing" element={<PricingPage />} />
           <Route path="/invoice-demo" element={<InvoiceDemo />} />
           <Route path="/admin/*" element={<AdminDashboard />} />
           <Route path="/customer/*" element={<CustomerPortal />} />
